35991102a75a1d3165c1ed86fd2c3828bbd06ceb
4kills <dominik.ochs@gmx.net>
2020-02-02 01:23:25 +0100

    added example

---
+++ examples/SomeTest.java
@@ -0,0 +1,51 @@
+import net._4kills.kit.clitester.*;
+import org.junit.jupiter.api.*;
+
+import java.util.function.Consumer;
+
+class MainTest {
+    @Test
+    void someTest () {
+        // the main method of your application
+        Consumer<String[]> mainMethod = (e) -> net._4kills.kit.somepackage.Main.main(e);
+
+        // tests the given application against the provided commands in the provided order.
+        // Thus 'lol' is fed first to the application, followed by 'start  torus' etc.
+        // The result contains all the outputs of the application to stdout.
+        Result actual = Tester.testAllCmds(mainMethod, "lol", "start  torus", "badg", "start torus", "quit");
+
+        // now you can declare your expected results. The Result.ERR matches any output beginning with "Error, ".
+        // So in this example we expect 3 errors followed by exactly on "OK" output.
+        // After that we expect the app to exit due to the "quit" command. So there shouldn't be any output!
+        Result expected = new Result(Result.ERR, Result.ERR, Result.ERR, "OK");
+
+        // finally the assertEquals tests if the actual and expected result are equal, in the sense that
+        // all their entries are the same except for error-msgs in the form of "Error, ".
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // you could also set this as static attribute of your test class so you dont have to assign
+    // it every time.
+    static Consumer<String[]> mainMethod = (e) -> net._4kills.kit.somepackage.Main.main(e);
+
+    @Test
+    void someOtherTest () {
+        // we can also test our application by providing additional command line arguments that will be
+        // given to the main application.
+        String[] args = new String[] {"argument1", "argument2"};
+        // even though it is strongly recommanded to always use a "quit" call at the end you don't need to
+        // call it explicitly! If you don't call it the main method will timeout after the period specified in
+        // Tester.getTimeout()
+        Result actual = Tester.testAll(mainMethod, args, "lol", "start  torus", "badg", "bs", "bs2");
+
+        // this time we expect 5 errors, but instead of having to type them all out we can use the following overload:
+        Result expected = new Result(5);
+        // = new Result(Result.ERR, Result.ERR, Result.ERR, Result.ERR, Result.ERR);
+
+        // There is also a mixed constructor that first takes n errors and then other result entries:
+        // Result expected = new Result(3, "OK");
+
+        // With junit 5 we can even see the actual results and the expected ones if the tests fail.
+        Assertions.assertEquals(expected, actual);
+    }
+}
---
+++ src/main/net/_4kills/kit/clitester/Tester.java
@@ -0,0 +1,132 @@
+package net._4kills.kit.clitester;
+
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.function.Consumer;
+
+public abstract class Tester {
+
+    private static long timeout = 1000;
+
+    /**
+     * timeout specifies how long to wait for the main method to terminate. This is needed when no 'quit' command
+     * is provided. The main method is forcefully terminated after the specified time in <b>ms</b> has passed.
+     * Defaults to 1000 ms.
+     * <p>
+     *     If you want the possibly fastest execution please use a "quit" command.
+     * </p>
+     *
+     * @param t Timeout in ms. Must be greater than 30 ms but smaller than 60 000 ms (1 min).
+     */
+    public static void setTimeout(long t) {
+        if (t < 30 || t > 60000) return;
+        timeout = t;
+    }
+
+    /**
+     * timeout specifies how long to wait for the main method to terminate. This is needed when no 'quit' command
+     * is provided. The main method is forcefully terminated after the specified time in <b>ms</b> has passed.
+     * Defaults to 1000 ms.
+     * <p>
+     *     If you want the possibly fastest execution please use a "quit" command.
+     * </p>
+     *
+     */
+    public static long getTimeout() {
+        return timeout;
+    }
+
+    /**
+     * testAllCmds runs the provided main method with no arguments and then sequentially feeds the provided commands
+     * to stdin. Then it collects the results of main from stdout and returns them line-wise as String[].
+     * @param main The main method to be executed
+     * @param cmds The line-wise commands to provide to main
+     * @return The results main produced in the same order as the provided cmds
+     */
+    public static Result testAllCmds(@NotNull Consumer<String[]> main, @NotNull String... cmds) {
+        return testAll(main, null, cmds);
+    }
+
+    /**
+     * testAllArgs runs the provided main method with the provided arguments.
+     * Then it collects the results of main from stdout and returns them line-wise as String[].
+     * @param main The main method to be executed
+     * @param args The command line arguments provided to main
+     * @return The results main produced for the given args
+     */
+    public static Result testAllArgs(@NotNull Consumer<String[]> main, @NotNull String... args) {
+        return new Result(executeMain(main, args));
+    }
+
+    /**
+     * testAll runs the provided main method with the provided args and then sequentially feeds the provided commands
+     * to stdin. Then it collects the results of main from stdout and returns them line-wise as String[].
+     * @param main The main method to be executed
+     * @param args The command line arguments provided to main
+     * @param cmds The line-wise commands to provide to main
+     * @return The results main produced in the same order as the provided cmds
+     */
+    public static Result testAll(@NotNull Consumer<String[]> main, @Nullable String[] args,
+                                   @Nullable String... cmds) {
+        InputStream in = new ByteArrayInputStream(normalizeInput(cmds).getBytes());
+        InputStream oldIn = System.in;
+        System.setIn(in);
+
+        Result out = new Result(executeMain(main, args));
+
+        System.setIn(oldIn);
+        return out;
+    }
+
+    /**
+     * starts the main with provided arguments in a new thread that times out after the period specified
+     * by {@link #timeout}. Returns the results in stdout
+     */
+    private static String[] executeMain (Consumer<String[]> main, String[] args) {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(output);
+        PrintStream oldOut = System.out;
+        System.setOut(out);
+
+        Thread t = new Thread(() -> {
+            try{
+                main.accept(args);
+            } catch (NullPointerException e) {
+                /* this happens when no "quit" statement is provided, main is interrupted by join
+                 and the main class reads null from the input stream.
+                 I can't fix this because I have no access to the main method.
+                 If the NullPointerException does not originate from the above mentioned cause, the test will fail
+                */
+            }
+        });
+        t.start();
+
+        try{
+            t.join(timeout);
+        } catch(InterruptedException e) {
+            e.printStackTrace(); // this shouldn't happen
+        }
+
+        System.out.flush();
+        System.setOut(oldOut);
+        return output.toString().split("\r\n");
+    }
+
+    /**
+     * Adds a newline after each provided command.
+     * @param cmds The commands to be modified
+     * @return The commands with newlines
+     */
+    private static String normalizeInput(String[] cmds) {
+        StringBuilder str = new StringBuilder();
+        for (String s : cmds) {
+            str.append(s).append("\n");
+        }
+        return str.toString();
+    }
+}
