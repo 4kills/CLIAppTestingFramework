a0316c19d89122ac029914ddbe74d56062193ee6
BuildTools <dominik.ochs@gmx.net>
2020-02-28 14:42:39 +0100

    added options to monitor timeouts

--- src/main/net/_4kills/kit/clitester/Tester.java
+++ src/main/net/_4kills/kit/clitester/Tester.java
@@ -7,7 +7,50 @@ import java.io.PrintStream;
 import java.util.function.Consumer;
 
 public abstract class Tester {
+    private final static String TIMEOUT_INDICATION = "---TIMEOUT---";
+
     private static long timeout = 1000;
+    private static boolean hasTimeouted;
+    private static boolean printTimeouted = false;
+
+    /**
+     * Gets whether a timeout should be reflected in the actual Result provided by
+     * {@link #testAll(Consumer, String[], String...)}, {@link #testAllCmds(Consumer, String...)} and
+     * {@link #testAllArgs(Consumer, String...)}.<br>
+     * <br>If this is true, the above mentioned methods will add: <p>---TIMEOUT---</p> to the list of contents of
+     * the Result which is returned, to indicate that a timeout has occurred.
+     * <br>Alternatively one may call {@link #hasTimeouted()} to find out whether a timeout occurred
+     * <br>Default is false
+     *
+     * @return true if ---TIMEOUT--- should be added to the result
+     */
+    public static boolean getPrintTimeouted() {
+        return printTimeouted;
+    }
+
+    /**
+     * Sets whether a timeout should be reflected in the actual Result provided by
+     * {@link #testAll(Consumer, String[], String...)}, {@link #testAllCmds(Consumer, String...)} and
+     * {@link #testAllArgs(Consumer, String...)}.<br>
+     * <br>If set to true, the above mentioned methods will add: <p>---TIMEOUT---</p> to the list of contents of
+     * the Result which is returned, to indicate that a timeout has occurred.
+     * <br>Default is false
+     *
+     * @param printTimeouted if true: ---TIMEOUT--- is added to the result.
+     */
+    public static void setPrintTimeouted(boolean printTimeouted) {
+        Tester.printTimeouted = printTimeouted;
+    }
+
+    /**
+     * Returns whether the previous call to test the main method has timeouted in the sense of
+     * {@link #setTimeout(long)} and {@link #getTimeout()}.
+     *
+     * @return true if it has timeouted, false otherwise
+     */
+    public static boolean hasTimeouted() {
+        return hasTimeouted;
+    }
 
     /**
      * Timeout specifies how long to wait for the main method to terminate.
@@ -92,12 +135,13 @@ public abstract class Tester {
         PrintStream oldOut = System.out;
         System.setOut(out);
 
+        hasTimeouted = true;
         Thread t = new Thread(() -> {
             try {
                 main.accept(args);
             } catch (EndOfStreamException ignored) {
-
             }
+            hasTimeouted = false;
         });
         t.start();
 
@@ -110,6 +154,7 @@ public abstract class Tester {
         System.out.flush();
         System.setOut(oldOut);
         String ret = output.toString().replace("\r", "");
+        if (getPrintTimeouted() && hasTimeouted()) ret = ret.concat(TIMEOUT_INDICATION);
         return ret.split("\n");
     }
 
